ff4b4a5830d20096a35ea4b3a695bdb68406fe2b
Vainslav <Vladik_ww@mail.ru>
2024-07-15 16:49:23 +0300

    SuperRegular script

---
+++ perks/mobility/super_regular.gd
@@ -0,0 +1,15 @@
+class_name SuperRegular extends RegularMovement
+
+var number_of_jumps = 4
+
+func handle_vertical_movement(delta: float) -> void:
+	if player.is_on_floor():
+		number_of_jumps = 4
+	if Input.is_action_just_pressed("jump") and number_of_jumps > 0:
+		try_jump()
+		number_of_jumps -= 1
+	player.velocity.y = minf(TERMINAL_VELOCITY, player.velocity.y + gravity * delta)
+
+func handle_horizontal_movement(delta: float) -> void:
+	var direction := Input.get_axis("move_left", "move_right") * WALK_SPEED * 3
+	player.velocity.x = move_toward(player.velocity.x, direction, ACCELERATION_SPEED * delta)
