3d955e177f7455b25964fa9fb071c580aaf71e7f
Simon Markov <semyonmarkov@mail.ru>
2024-07-15 20:29:11 +0300

    added maxcharge duratioon

--- perks/mobility/megadash.gd
+++ perks/mobility/megadash.gd
@@ -1,6 +1,8 @@
 class_name Megadash extends RegularMovement
 
 var in_charge = false
+var charge_time = 0
+var MAX_CHARGE_DURATION = 1
 var target:Vector2
 var start:Vector2
 func _physics_process(delta: float) -> void:
@@ -11,8 +13,9 @@ func _physics_process(delta: float) -> void:
 		megadash(delta);
 		
 	if in_charge:
+		charge_time -= delta
 		player.velocity = ((target - player.get_position())).normalized()*3000
-		if (target - start).dot(target - player.get_position()) < 0:
+		if (target - start).dot(target - player.get_position()) < 0 or charge_time < 0:
 			in_charge = false
 			player.velocity = Vector2.ZERO
 		
@@ -35,6 +38,7 @@ func megadash(delta: float):
 	player.get_stunned(1)
 	await get_tree().create_timer(1).timeout
 	in_charge = true
+	charge_time = MAX_CHARGE_DURATION
 	target = player.get_global_mouse_position()
 	start = player.get_position()
 	
