8724146755bc6805b683499ebdae31eede40d3c9
koldun <eg.sharypov@ya.ru>
2024-07-16 21:02:00 +0300

    kvadrat now kills enemies

--- perks/combat/kvadrat/cell.gd
+++ perks/combat/kvadrat/cell.gd
@@ -3,5 +3,12 @@ class_name Cell extends Area2D
 @onready var inactive_texture = preload("res://perks/combat/kvadrat/inactive.png")
 @onready var active_texture = preload("res://perks/combat/kvadrat/active.png")
 
+func _ready():
+	set_active(false)
+
 func set_active(active):
+	monitoring = active
 	$Sprite.texture = active_texture if active else inactive_texture
+
+func _on_body_entered(body):
+	body.queue_free()
