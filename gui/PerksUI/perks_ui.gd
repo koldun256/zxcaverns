eda05f1f845da3e3327f78b4b669422c401a6838
Simon Markov <semyonmarkov@mail.ru>
2024-07-17 12:46:30 +0300

    vse propalo, no vernulosb, bugfix

--- gui/PerksUI/perks_ui.gd
+++ gui/PerksUI/perks_ui.gd
@@ -1,8 +1,7 @@
 class_name PerksUI extends HBoxContainer
 
-@onready var perk_ui_scene = preload("res://gui/PerksUI/perk_ui.tscn")
-
 func _on_perks_on_set_perks(perks):
+	var perk_ui_scene = load("res://gui/PerksUI/perk_ui.tscn")
 	for perk in perks:
 		var perk_ui = perk_ui_scene.instantiate()
 		perk_ui.texture = perk.icon
